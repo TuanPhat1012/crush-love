f9a3513c43b41044feb107b9a1586fa14ce961ee
TuanPhat1012 <94225176+TuanPhat1012@users.noreply.github.com>
2021-11-13 19:31:59 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -6,5 +6,6 @@ const CONFIGDATA = {
   buttonNo: "Không! Còn lâu.",
   titleModar: "Cảm ơn em vì đồng ý",
   descriptionModar:
-    'Anh biết là em sẽ đồng ý mà ^^ </br> Cám ơn em đã đồng ý để cho anh bảo vệ và yêu em.Đối với anh, em mãi là em bé của anh </br> ",
+    'Anh biết là em sẽ đồng ý mà ^^ </br> Cám ơn em đã đồng ý để cho anh bảo vệ và yêu em.Đối với anh, em mãi là em bé của anh </br> "',
+};
  
