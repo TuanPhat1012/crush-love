7853a036cf6329ff8987d7c48092b6d7b89123ae
TuanPhat1012 <94225176+TuanPhat1012@users.noreply.github.com>
2021-11-13 23:43:37 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -1,11 +1,11 @@
 const CONFIGDATA = {
-  titleHeader: "Em có yêu anh không?",
+  titleHeader: "Em đồng ý quay lại với anh được ko?",
   descriptionHeader:
     "Nếu em thoát ra và không trả lời thì có nghĩa là em đã ĐỒNG Ý rồi đấy ",
-  buttonYes: "Có",
+  buttonYes: "đồng ý",
   buttonNo: "Không! Còn lâu.",
-  titleModar: "Cảm ơn em vì đồng ý",
+  titleModar: "iu đoàn ngọc quỳnh như",
   descriptionModar:
-    'Anh biết là em sẽ đồng ý mà ^^ </br> Cám ơn em đã đồng ý để cho anh bảo vệ và yêu em.Đối với anh, em mãi là em bé của anh </br> "',
+    'Anh biết là em sẽ đồng ý mà  </br> Cám ơn em đã đồng ý để cho anh bảo vệ và yêu em.Đối với anh, em mãi là em bé của anh </br> "',
 };
  
